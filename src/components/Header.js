24932eb42fb786b427c42b020b8c25549611e42d
Rafi Raihansyah <13519154@std.stei.itb.ac.id>
2021-10-12 18:03:21 +0700

    Finished navbar appearance

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,65 @@
-import { AppBar, Toolbar, Typography } from '@mui/material';
-import { makeStyles } from '@mui/styles';
-import { fontWeight } from '@mui/system';
 import React from 'react';
+import { AppBar, Toolbar, Typography } from '@mui/material';
+import { Button } from '@mui/material';
+import { makeStyles } from '@mui/styles';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
 const useStyles = makeStyles({
   logo: {
-    width: '70.96px',
-    height: '72px',
-    background: 'url(./assets/images/logo najait item.png)',
+    width: '54px',
+    height: '54px',
+  },
+  'logo-container': {
+    display: 'flex',
+    alignItems: 'center',
+    cursor: 'pointer',
   },
 });
 
 // Najait logo
 const Logo = () => {
   const classes = useStyles();
-  return <div className={classes.logo}></div>;
+  return (
+    <div className={classes['logo-container']}>
+      <img
+        src="./assets/images/logo najait item.png"
+        alt="logo"
+        className={classes.logo}
+      />
+      <Typography
+        sx={{
+          color: '#266679',
+          fontFamily: `"Playfair Display", serif`,
+          fontWeight: 700,
+          fontSize: '1.5em',
+          marginRight: '54px',
+        }}
+      >
+        Najait
+      </Typography>
+    </div>
+  );
+};
+
+// Nav item
+const NavItem = ({ text }) => {
+  return (
+    <Typography
+      sx={{
+        fontFamily: `"Playfair Display", serif`,
+        marginX: '18px',
+        padding: '6px 12px',
+        cursor: 'pointer',
+        borderRadius: '8px',
+        '&:hover': {
+          backgroundColor: '#f0f0f0',
+        },
+      }}
+      onClick={() => console.log(text, ' clicked')}
+    >
+      {text}
+    </Typography>
+  );
 };
 
 const Header = () => {
@@ -26,17 +71,43 @@ const Header = () => {
           backgroundColor: 'white',
         }}
       >
+        {/* Home logo */}
         <Logo />
-        <Typography
+
+        {/* Nav item(s) */}
+        <NavItem text="Profil Penjait" />
+
+        {/* Cart page */}
+        <ShoppingCartIcon
           sx={{
+            marginLeft: 'auto',
             color: '#266679',
-            fontFamily: `"Playfair Display", serif`,
+            cursor: 'pointer',
+            borderRadius: '64px',
+            padding: '8px',
+            '&:hover': {
+              backgroundColor: '#f0f0f0',
+            },
+          }}
+        />
+
+        {/* Sign in button */}
+        <Button
+          sx={{
+            margin: '0 18px 0 36px',
             fontWeight: 700,
-            fontSize: '36px',
+            color: 'white',
+            backgroundColor: '#266679',
+            textTransform: 'capitalize',
+            '&:hover': {
+              color: 'white',
+              backgroundColor: '#266679',
+              filter: 'brightness(85%)',
+            },
           }}
         >
-          Najait
-        </Typography>
+          Masuk
+        </Button>
       </Toolbar>
     </AppBar>
   );
