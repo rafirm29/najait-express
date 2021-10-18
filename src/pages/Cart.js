f43c0f236a58d1e0afaff9312d6bdcd0ef487efe
Rafi Raihansyah <13519154@std.stei.itb.ac.id>
2021-10-19 00:43:55 +0700

    Added total pesanan card

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,11 +1,65 @@
-import { Typography } from '@mui/material';
+import {
+  Button,
+  Card,
+  CardContent,
+  CardHeader,
+  Container,
+  Grid,
+  Typography,
+  Box,
+} from '@mui/material';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import React from 'react';
 
 const Cart = () => {
   return (
-    <div>
-      <Typography>Cart page</Typography>
-    </div>
+    <Container>
+      <Typography variant="h4" my={3}>
+        Keranjang
+      </Typography>
+      <Grid container columnSpacing={5}>
+        <Grid item xs={12} sm={7}>
+          <Card>Cart item</Card>
+        </Grid>
+        <Grid item xs={12} sm={5}>
+          <Card>
+            <CardContent>
+              <Box borderBottom={1} borderColor="#C4C4C4">
+                <Typography gutterBottom variant="h6">
+                  Total Pesanan
+                </Typography>
+              </Box>
+              <Box borderBottom={1} borderColor="#C4C4C4">
+                <Grid container my={2}>
+                  <Grid item xs>
+                    <Typography fontWeight="bold">Subtotal</Typography>
+                  </Grid>
+                  <Grid item>
+                    <Typography>Rp90.000</Typography>
+                  </Grid>
+                </Grid>
+              </Box>
+              <Box>
+                <Grid container my={2}>
+                  <Grid item xs>
+                    <Typography fontWeight="bold">Total</Typography>
+                  </Grid>
+                  <Grid item>
+                    <Typography>Rp90.000</Typography>
+                  </Grid>
+                </Grid>
+              </Box>
+              <Button variant="contained" color="primary">
+                <Typography variant="body2" mr={1}>
+                  Check out
+                </Typography>
+                <ArrowForwardIcon />
+              </Button>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
+    </Container>
   );
 };
 
