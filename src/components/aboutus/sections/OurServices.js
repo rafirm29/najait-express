5e22067a0a4c3e9bd9d0d9aa6bde694161523bc4
Rafi Raihansyah <13519154@std.stei.itb.ac.id>
2021-12-04 20:44:29 +0700

    Finished about us 'our services' section

--- src/components/aboutus/sections/OurServices.js
+++ src/components/aboutus/sections/OurServices.js
@@ -1,13 +1,22 @@
-import { Grid, Box, Typography } from '@mui/material';
+import {
+  Grid,
+  Box,
+  Typography,
+  Card,
+  CardMedia,
+  CardContent,
+} from '@mui/material';
 import SectionTitle from '../SectionTitle';
 import React from 'react';
 
 const OurServices = () => {
   return (
     <Grid
+      container
       sx={{
         display: 'flex',
-        justifyContent: 'center',
+        flexDirection: 'column',
+        alignItems: 'center',
         background: `linear-gradient(rgba(255,255,255,0.5), rgba(255,255,255,0.5)), url("./assets/images/about-us-2.png")`,
         backgroundSize: 'cover',
         backgroundRepeat: 'no-repeat',
@@ -15,6 +24,35 @@ const OurServices = () => {
       }}
     >
       <SectionTitle text="Our Services" />
+      <Grid item xs={6} sm={4} md={3} mt={3}>
+        <Card>
+          <Box px={6} pt={3} mx={5} mt={3}>
+            <CardMedia
+              component="img"
+              image="./assets/images/najait-express.png"
+              alt="Najait Express"
+            />
+          </Box>
+          <CardContent>
+            <Typography
+              variant="h6"
+              fontFamily="Montserrat, sans-serif"
+              textAlign="center"
+              color="primary"
+              fontWeight="bold"
+              gutterBottom
+            >
+              Express
+            </Typography>
+            <Typography variant="subtitle2">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Temporibus, ratione recusandae id sit corporis iure ullam nam
+              exercitationem fuga pariatur quas non consectetur repudiandae
+              suscipit similique? Asperiores voluptatum a corporis.
+            </Typography>
+          </CardContent>
+        </Card>
+      </Grid>
     </Grid>
   );
 };
