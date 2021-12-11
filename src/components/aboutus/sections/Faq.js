3036d9727dbc9b57c73c75f0a2f06d530d8cd1f9
Rafi Raihansyah <13519154@std.stei.itb.ac.id>
2021-12-11 18:14:03 +0700

    Added FaQ section in about us

--- src/components/aboutus/sections/Faq.js
+++ src/components/aboutus/sections/Faq.js
@@ -1,17 +1,113 @@
-import { Grid } from '@mui/material';
-import React from 'react';
+import {
+  Grid,
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+  Typography,
+  Box,
+} from '@mui/material';
+import ArrowForwardIosSharpIcon from '@mui/icons-material/ArrowForwardIosSharp';
+import React, { useState } from 'react';
 import SectionTitle from '../SectionTitle';
 
+const faqList = [
+  {
+    question: 'Question 1',
+    answer: `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+  Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
+  eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+  Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
+  eget.`,
+  },
+  {
+    question: 'Question 2',
+    answer: `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+  Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
+  eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+  Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
+  eget.`,
+  },
+  {
+    question: 'Question 3',
+    answer: `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+  Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
+  eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+  Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
+  eget.`,
+  },
+  {
+    question: 'Question 4',
+    answer: `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+  Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
+  eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+  Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
+  eget.`,
+  },
+  {
+    question: 'Question 5',
+    answer: `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+  Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
+  eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+  Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
+  eget.`,
+  },
+  {
+    question: 'Question 6',
+    answer: `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+  Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
+  eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+  Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
+  eget.`,
+  },
+];
+
 const Faq = () => {
+  const [expanded, setExpanded] = useState(false);
+  const handleChange = (panel) => (event, newExpanded) => {
+    setExpanded(newExpanded ? panel : false);
+  };
+
+  let panel = 0;
   return (
     <Grid
       sx={{
         display: 'flex',
-        justifyContent: 'center',
-        height: '100vh',
+        flexDirection: 'column',
+        alignItems: 'center',
+        minHeight: '100vh',
       }}
     >
       <SectionTitle text="Frequently Asked Questions" dark={true} />
+      <Box p={2} m={1}>
+        {faqList.map((faq) => {
+          panel++;
+          const currentPanel = 'panel' + panel;
+          return (
+            <Accordion
+              expanded={expanded === currentPanel}
+              onChange={handleChange(currentPanel)}
+            >
+              <AccordionSummary
+                expandIcon={
+                  <ArrowForwardIosSharpIcon
+                    sx={{ transform: 'rotate(90deg)' }}
+                  />
+                }
+                aria-controls={currentPanel + 'd-content'}
+                id={currentPanel + 'd-header'}
+                sx={{
+                  backgroundColor: '#F5F5F5',
+                }}
+              >
+                <Typography fontWeight="bold">{faq.question}</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography>{faq.answer}</Typography>
+              </AccordionDetails>
+            </Accordion>
+          );
+        })}
+      </Box>
     </Grid>
   );
 };
