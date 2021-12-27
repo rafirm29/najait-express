bb7db46d3cf45152d18e3a1dd173eb98986b99dd
haikallf <haikallzrd@gmail.com>
2021-12-27 12:43:25 +0700

    header drawer

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,9 @@
 import React from "react";
-import { AppBar, Toolbar, Typography, Button } from "@mui/material";
+import { AppBar, Toolbar, Typography, Button, Drawer } from "@mui/material";
 import { Box } from "@mui/system";
 import { NavLink } from "react-router-dom";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import MenuIcon from "@mui/icons-material/Menu";
 import Logo from "./Logo";
 import { makeStyles } from "@mui/styles";
 
@@ -26,8 +27,8 @@ const NavItem = ({ text }) => {
         },
         mx: { xs: 0.5, md: 1.5 },
         px: { xs: 0.5, md: 2 },
-        py: { xs: 0.5, md: 1 },
-        fontSize: { xs: "14px" },
+        py: { xs: 1 },
+        fontSize: { xs: "16px" },
       }}
       onClick={() => console.log(text, " clicked")}
     >
@@ -38,6 +39,24 @@ const NavItem = ({ text }) => {
 
 const Header = () => {
   const classes = useStyles();
+
+  const [state, setState] = React.useState({
+    top: false,
+    left: false,
+    bottom: false,
+    right: false,
+  });
+
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+
+    setState({ ...state, [anchor]: open });
+  };
   return (
     <AppBar position="static">
       <Toolbar
@@ -46,6 +65,36 @@ const Header = () => {
           backgroundColor: "white",
         }}
       >
+        <Box sx={{ display: { xs: "block", md: "none" } }}>
+          <Button onClick={toggleDrawer("left", true)}>
+            <MenuIcon />
+          </Button>
+          <Drawer
+            anchor="left"
+            open={state["left"]}
+            onClose={toggleDrawer("left", false)}
+          >
+            <Box sx={{ px: { xs: 3, md: 5 } }}>
+              <Box mr={2} ml={1} my={2} sx={{ cursor: "pointer" }}>
+                <NavLink to="/home" className={classes.navlink}>
+                  <Logo color="black" textColor="primary" />
+                </NavLink>
+              </Box>
+              <Box mx={3} my={2}>
+                <NavLink to="/aboutus" className={classes.navlink}>
+                  <NavItem text="About Us" />
+                </NavLink>
+                <NavLink to="/profilpenjahit" className={classes.navlink}>
+                  <NavItem text="Profil Penjait" />
+                </NavLink>
+                <NavLink to="/ordersaya" className={classes.navlink}>
+                  <NavItem text="Order Saya" />
+                </NavLink>
+              </Box>
+            </Box>
+          </Drawer>
+        </Box>
+
         {/* Home logo */}
         <Box
           mr={2}
@@ -58,16 +107,17 @@ const Header = () => {
         </Box>
 
         {/* Nav item(s) */}
-
-        <NavLink to="/aboutus" className={classes.navlink}>
-          <NavItem text="About Us" />
-        </NavLink>
-        <NavLink to="/profilpenjahit" className={classes.navlink}>
-          <NavItem text="Profil Penjait" />
-        </NavLink>
-        <NavLink to="/ordersaya" className={classes.navlink}>
-          <NavItem text="Order Saya" />
-        </NavLink>
+        <Box display="flex" sx={{ display: { xs: "none", md: "flex" } }}>
+          <NavLink to="/aboutus" className={classes.navlink}>
+            <NavItem text="About Us" />
+          </NavLink>
+          <NavLink to="/profilpenjahit" className={classes.navlink}>
+            <NavItem text="Profil Penjait" />
+          </NavLink>
+          <NavLink to="/ordersaya" className={classes.navlink}>
+            <NavItem text="Order Saya" />
+          </NavLink>
+        </Box>
 
         {/* Cart page */}
         <Box ml="auto">
