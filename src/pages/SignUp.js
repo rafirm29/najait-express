7d862db1813f138a41c7fb9dc4de94c08c80b1d2
Rafi Raihansyah <13519154@std.stei.itb.ac.id>
2021-12-30 13:57:41 +0700

    added sign up form validation

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import MuiLink from '@mui/material/Link';
 import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
+import Alert from '@mui/material/Alert';
 import { LoadingButton } from '@mui/lab';
 import axios from 'axios';
 import CONFIG from '../config';
@@ -17,8 +17,123 @@ const SignUp = () => {
   const auth = useAuth();
   const history = useHistory();
   const [loading, setLoading] = useState(false);
+  const [feedback, setFeedback] = useState(null);
+
+  const [error, setError] = useState({
+    firstName: null,
+    lastName: null,
+    email: null,
+    password: null,
+    address: null,
+    phone: null,
+  });
+  const [formData, setFormData] = useState({
+    firstName: null,
+    lastName: null,
+    email: null,
+    password: null,
+    address: null,
+    phone: null,
+  });
+
+  // Checking for validation errors in the form
+  // Returns true if there's error
+  const checkError = () => {
+    for (const field in error) {
+      if (Object.hasOwnProperty.call(error, field)) {
+        if (!error[field]) {
+          return true;
+        }
+      }
+    }
+    return false;
+  };
+
+  const handleChange = (e) => {
+    const target = e.target;
+    switch (target.name) {
+      case 'firstName':
+        if (target.value.length === 0) {
+          setError({ ...error, firstName: 'First name cannot be empty' });
+        } else if (target.value.length > 20) {
+          setError({
+            ...error,
+            firstName: 'First name cannot be more than 20 characters',
+          });
+        } else if (!/^[a-z ,.'-]+$/i.test(target.value)) {
+          setError({ ...error, firstName: 'Please enter a valid name' });
+        } else {
+          setError({ ...error, firstName: null });
+        }
+        setFormData({ ...formData, firstName: target.value });
+        break;
+      case 'lastName':
+        if (target.value.length === 0) {
+          setError({ ...error, lastName: 'Last name cannot be empty' });
+        } else if (target.value.length > 20) {
+          setError({
+            ...error,
+            lastName: 'Last name cannot be more than 20 characters',
+          });
+        } else if (!/^[a-z ,.'-]+$/i.test(target.value)) {
+          setError({ ...error, lastName: 'Please enter a valid name' });
+        } else {
+          setError({ ...error, lastName: null });
+        }
+        setFormData({ ...formData, lastName: target.value });
+        break;
+      case 'email':
+        if (target.value.length === 0) {
+          setError({ ...error, email: 'Email cannot be empty' });
+        } else if (
+          !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(target.value)
+        ) {
+          setError({ ...error, email: 'Please enter a valid email' });
+        } else {
+          setError({ ...error, email: null });
+        }
+        setFormData({ ...formData, email: target.value });
+        break;
+      case 'password':
+        if (target.value.length < 8) {
+          setError({
+            ...error,
+            password: 'Password must be at least 8 characters long',
+          });
+        } else {
+          setError({ ...error, password: null });
+        }
+        setFormData({ ...formData, password: target.value });
+        break;
+      case 'address':
+        if (target.value.length === 0) {
+          setError({ ...error, address: 'Address cannot be empty' });
+        } else {
+          setError({ ...error, address: null });
+        }
+        setFormData({ ...formData, address: target.value });
+        break;
+      case 'phone':
+        if (target.value.length === 0) {
+          setError({ ...error, phone: 'Phone number cannot be empty' });
+        } else if (
+          !/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,8}$/im.test(
+            target.value
+          )
+        ) {
+          setError({ ...error, phone: 'Please enter a valid phone number' });
+        } else {
+          setError({ ...error, phone: null });
+        }
+        setFormData({ ...formData, phone: e.target.value });
+        break;
+      default:
+        break;
+    }
+  };
 
   useEffect(() => {
+    checkError();
     if (auth.isAuthenticated()) {
       history.replace('/home');
     }
@@ -35,16 +150,35 @@ const SignUp = () => {
       address: data.get('address'),
       phone: data.get('phone'),
     };
-    console.log(payload);
     setLoading(true);
+    setFeedback(null);
     try {
       const response = await axios.post(
         `${CONFIG.API_URL}/user/register`,
         payload
       );
-      console.log(response.data);
+      if (response.data.error === -2) {
+        // Error email is taken
+        setFeedback({
+          type: 'error',
+          msg: 'Email is already taken. Please try a different email',
+        });
+        setError({ ...error, email: 'Email is already taken' });
+      } else {
+        setFeedback({
+          type: 'success',
+          msg: 'Success',
+        });
+        setTimeout(() => {
+          window.location.href = '/login';
+        }, 1500);
+      }
     } catch (err) {
       console.error(err);
+      setFeedback({
+        type: 'error',
+        msg: 'Please fill in the fields correctly',
+      });
     } finally {
       setLoading(false);
     }
@@ -79,25 +213,37 @@ const SignUp = () => {
             src="./assets/images/logo-black.png"
             sx={{ m: 1 }}
           />
-          <Typography component="h1" variant="h5" fontFamily="Montserrat">
+          <Typography
+            component="h1"
+            variant="h5"
+            fontFamily="Montserrat"
+            gutterBottom
+          >
             Sign up
           </Typography>
+          {feedback ? (
+            <Alert severity={feedback.type}>{feedback.msg}</Alert>
+          ) : null}
           <Box
             component="form"
             noValidate
             onSubmit={handleSubmit}
+            autoComplete="off"
             sx={{ mt: 3 }}
           >
             <Grid container spacing={2}>
               <Grid item xs={12} sm={6}>
                 <TextField
-                  autoComplete="given-name"
                   name="firstName"
                   required
                   fullWidth
                   id="firstName"
                   label="First Name"
                   autoFocus
+                  error={error.firstName}
+                  helperText={error.firstName && error.firstName}
+                  value={formData.firstName}
+                  onChange={handleChange}
                 />
               </Grid>
               <Grid item xs={12} sm={6}>
@@ -107,7 +253,10 @@ const SignUp = () => {
                   id="lastName"
                   label="Last Name"
                   name="lastName"
-                  autoComplete="family-name"
+                  error={error.lastName}
+                  helperText={error.lastName && error.lastName}
+                  value={formData.lastName}
+                  onChange={handleChange}
                 />
               </Grid>
               <Grid item xs={12}>
@@ -117,7 +266,10 @@ const SignUp = () => {
                   id="email"
                   label="Email Address"
                   name="email"
-                  autoComplete="email"
+                  error={error.email}
+                  helperText={error.email && error.email}
+                  value={formData.email}
+                  onChange={handleChange}
                 />
               </Grid>
               <Grid item xs={12}>
@@ -128,7 +280,10 @@ const SignUp = () => {
                   label="Password"
                   type="password"
                   id="password"
-                  autoComplete="new-password"
+                  error={error.password}
+                  helperText={error.password && error.password}
+                  value={formData.password}
+                  onChange={handleChange}
                 />
               </Grid>
               <Grid item xs={12}>
@@ -139,6 +294,10 @@ const SignUp = () => {
                   name="address"
                   label="Address"
                   id="address"
+                  error={error.address}
+                  helperText={error.address && error.address}
+                  value={formData.address}
+                  onChange={handleChange}
                 />
               </Grid>
               <Grid item xs={12}>
@@ -148,12 +307,16 @@ const SignUp = () => {
                   name="phone"
                   label="Phone"
                   id="phone"
+                  error={error.phone}
+                  helperText={error.phone && error.phone}
+                  value={formData.phone}
+                  onChange={handleChange}
                 />
               </Grid>
             </Grid>
             <LoadingButton
               loading={loading}
-              type="submit"
+              type={checkError() ? null : 'submit'}
               fullWidth
               variant="contained"
               sx={{ mt: 3, mb: 2 }}
